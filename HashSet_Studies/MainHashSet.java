9bc00b8be67eba0bbdd992f0939891908f8a4a38
Pedro Lazera Jr <pepelazera@gmail.com>
2026-04-15 16:56:05 -0300

    Update MainHashSet.java

--- HashSet_Studies/MainHashSet.java
+++ HashSet_Studies/MainHashSet.java
@@ -3,26 +3,38 @@ package HashSet_Studies;
 import java.util.*;
 
 public class MainHashSet {
-    public static void main() {
+    public static void main(String[] args) {
 
         List<String> ninjas = new ArrayList<>();
         ninjas.add("Naruto Uzumaki");
         ninjas.add("Sasuke Uchiha");
         ninjas.add("Sakura Haruno");
         ninjas.add("Naruto Uzumaki");
-        ninjas.remove(0);
+        ninjas.add("Danzo");
+        ninjas.add("Kakashi Hatake");
+        ninjas.add("Boruto Uzumaki");
 
         System.out.println("Ninjas: " + ninjas);
 
         // O set é usado para ignorar itens duplicados dentro da minha lista
-        Set<String> ninjasSet = new TreeSet<>();
-        ninjasSet.addAll(ninjas);
-        ninjasSet.remove("Sakura Haruno"); // No sets, é possível remover pela própria String
+        Set<String> ninjasHashSet = new HashSet<>(); // Tira os duplicados e não tem ordenação
+
+        ninjasHashSet.addAll(ninjas);
+        ninjasHashSet.remove("Sakura Haruno"); // No sets, é possível remover pela própria String
 
         // Nos sets, nós não trabalhamos com índices
-        // RELEMBRANDO: Sets não aceitam duplicatas
+        // RELEMBRANDO: sets não aceitam duplicatas
         // O set também não tem ordenação
-        System.out.println("Ninjas set: " + ninjasSet);
+        System.out.println("Ninjas HashSet: " + ninjasHashSet);
 
+        Set<String> ninjasTreeSet = new TreeSet<>(); // Usado quando quero utilizar uma ordem lógica (alfabética, numérica)
+
+        ninjasTreeSet.addAll(ninjas);
+        System.out.println("Ninjas TreeSet: " + ninjasTreeSet);
+
+        Set<String> ninjasLinkedHashSet = new LinkedHashSet<>(); // Irá organizar por ordem de implementação
+
+        ninjasLinkedHashSet.addAll(ninjas);
+        System.out.println("Ninjas LinkedHashSet: " + ninjasLinkedHashSet);
     }
 }
